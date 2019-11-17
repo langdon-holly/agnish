e69d810960cb736b6b959dc064e7ebeb18e48a08
Langdon Holly <langdonzh@gmail.com>
2019-11-17 15:18:22 -0500

    handle args to agnish; sooner syntax error; change some messages

--- agnish.rs
+++ agnish.rs
@@ -1,85 +1,108 @@
 use std::{
+    env::args_os,
     ffi::OsStr,
-    io::{stdin, stdout, Read, Write},
+    io::{stdin, stdout, Read, StdoutLock, Write},
+    mem::replace,
     os::unix::ffi::OsStrExt,
     process::Command,
 };
+
+struct ParsingState<'a> {
+    words: Vec<Box<[u8]>>,
+    current_word: Vec<u8>,
+    nesting_level: usize,
+    escaping: bool,
+    bad: bool,
+    stdout: StdoutLock<'a>,
+}
+
+fn handle_byte(state: &mut ParsingState, byte: u8) {
+    if state.bad {
+        if byte == b'\n' {
+            state.bad = false;
+            state.stdout.write(b"bad syntax\n[do]").unwrap();
+            state.stdout.flush().unwrap();
+        }
+    } else {
+        if state.nesting_level == 0 {
+            match byte {
+                b'[' => state.nesting_level = 1,
+                b'\n' => {
+                    if state.words.len() == 0 {
+                        if byte == b'\n' {
+                            state.stdout.write(b"bad syntax\n[do]").unwrap();
+                            state.stdout.flush().unwrap();
+                        }
+                    } else {
+                        let mut command = Command::new(OsStr::from_bytes(&*state.words[0]));
+                        for i in 1..state.words.len() {
+                            command.arg(OsStr::from_bytes(&*state.words[i]));
+                        }
+                        if let Err(_) = command.status() {
+                            state.stdout.write(b"unsuccessful\n").unwrap();
+                        }
+                        state.words = Vec::new();
+                        state.stdout.write(b"[do]").unwrap();
+                        state.stdout.flush().unwrap();
+                    }
+                }
+                _ => state.bad = true,
+            }
+        } else {
+            if state.escaping {
+                state.escaping = false;
+                state.current_word.push(byte);
+            } else {
+                match byte {
+                    b'[' => {
+                        state.nesting_level += 1;
+                        state.current_word.push(byte);
+                    }
+                    b']' => {
+                        state.nesting_level -= 1;
+                        if state.nesting_level == 0 {
+                            state.words.push(
+                                replace(&mut state.current_word, Vec::new()).into_boxed_slice(),
+                            );
+                        } else {
+                            state.current_word.push(byte);
+                        }
+                    }
+                    b'\\' => state.escaping = true,
+                    _ => state.current_word.push(byte),
+                }
+            }
+        }
+    }
+}
 
 fn main() {
     let the_unlocked_stdin = stdin();
     let mut the_stdin = the_unlocked_stdin.lock();
     let the_unlocked_stdout = stdout();
-    let mut the_stdout = the_unlocked_stdout.lock();
-    'OUTER: loop {
-        the_stdout.write(b"[run]").unwrap();
-        the_stdout.flush().unwrap();
 
-        let mut words: Vec<Box<[u8]>> = Vec::new();
-        let mut current_word: Vec<u8> = Vec::new();
-        let mut nesting_level = 0;
-        let mut escaping = false;
-        let mut bad = false;
-        loop {
-            let mut buf: [u8; 1] = Default::default();
-            if the_stdin.read(&mut buf).unwrap() == 0 {
-                break 'OUTER;
-            }
-            let byte = buf[0];
-            if nesting_level == 0 {
-                match byte {
-                    b'[' => {
-                        nesting_level = 1;
-                    }
-                    b'\n' => {
-                        if words.len() == 0 || bad {
-                            bad = false;
-                            the_stdout.write(b"bad syntax\n[run]").unwrap();
-                            the_stdout.flush().unwrap();
-                            words = Vec::new();
-                        } else {
-                            break;
-                        }
-                    }
-                    _ => {
-                        bad = true;
-                    }
-                }
-            } else {
-                if escaping {
-                    escaping = false;
-                    current_word.push(byte);
-                } else {
-                    match byte {
-                        b'[' => {
-                            nesting_level += 1;
-                            current_word.push(byte);
-                        }
-                        b']' => {
-                            nesting_level -= 1;
-                            if nesting_level == 0 {
-                                words.push(current_word.into_boxed_slice());
-                                current_word = Vec::new();
-                            } else {
-                                current_word.push(byte);
-                            }
-                        }
-                        b'\\' => {
-                            escaping = true;
-                        }
-                        _ => {
-                            current_word.push(byte);
-                        }
-                    }
-                }
-            }
-        }
+    let mut state = ParsingState {
+        words: Vec::new(),
+        current_word: Vec::new(),
+        nesting_level: 0,
+        escaping: false,
+        bad: false,
+        stdout: the_unlocked_stdout.lock(),
+    };
 
-        let mut command = Command::new(OsStr::from_bytes(&*words[0]));
-        for i in 1..words.len() {
-            command.arg(OsStr::from_bytes(&*words[i]));
+    state.stdout.write(b"[do]").unwrap();
+    state.stdout.flush().unwrap();
+
+    for arg in args_os().skip(1) {
+        for byte in arg.as_bytes() {
+            state.stdout.write(&[*byte]).unwrap();
+            state.stdout.flush().unwrap();
+            handle_byte(&mut state, *byte)
         }
-        if let Err(_) = command.status() {
-            the_stdout.write(b"wouldn't run\n").unwrap();
-        }
+    }
+
+    let mut buf: [u8; 1] = Default::default();
+    while the_stdin.read(&mut buf).unwrap() > 0 {
+        handle_byte(&mut state, buf[0])
     }
 }
